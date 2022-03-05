90c757da00545143c77e1bbb60dc755d80fcc00b
lonelybuddy <henryzheng92@gmail.com>
2022-03-05 11:14:01 -0600

    finished HTML structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ['14px', { letterSpacing: '-0.17px', lineHeight: '26px' }],
+    },
+    extend: {
+      colors: {
+        Cyan: 'hsl(179, 62%, 43%)',
+        'Bright-Yellow': 'hsl(71, 73%, 54%)',
+        'Light-Gray': 'hsl(204, 43%, 93%)',
+        'Grayish-Blue': 'hsl(218, 22%, 67%)',
+      },
+      fontFamily: {
+        karla: ['Karla', 'sans-serif'],
+      },
+      boxShadow: {
+        'join-community': '0px 15px 30px rgba(0, 81, 171, 0.148847)',
+      },
+    },
   },
   plugins: [],
 };
